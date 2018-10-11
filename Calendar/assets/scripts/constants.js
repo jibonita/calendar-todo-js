eb700877ba9a8a8cf6e97bcd3df0b65f8ccf2642
jibonita <bamishore@gmail.com>
2018-10-11 13:08:27 +0300

    created modules in calendar  + moved files to other folders

---
+++ Calendar/assets/scripts/constants.js
@@ -0,0 +1,12 @@
+//const constants = () => {
+    const CELL_PERCENTAGE_WIDTH = 14;
+    const CALENDAR_CONTAINER_ID = '#cal-container';
+//}
+
+//export {constants};
+export {
+  CELL_PERCENTAGE_WIDTH,
+  CALENDAR_CONTAINER_ID,
+};
+
+
