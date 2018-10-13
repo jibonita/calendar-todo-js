6fe85b506b73679bd5888131c76b5aba49863255
jibonita <bamishore@gmail.com>
2018-10-13 12:05:53 +0300

    added more records to database file

--- assets/js/databse.js
+++ assets/js/databse.js
@@ -1,5 +1,5 @@
 const toDosDataInfo = [{
-    key: '20181010',
+    key: '2018-10-10',
     toDo: [{
             value: 'buy tobacco',
             important: false,
@@ -12,6 +12,38 @@ const toDosDataInfo = [{
             value: 'chill',
             important: false,
         },
-    ],
+    ]
+},
+{
+    key: '2018-10-13',
+    toDo: [{
+            value: 'team meet at the Academy',
+            important: false,
+        },
+        {
+            value: 'Stef upload latest changes',
+            important: false,
+        },
+        {
+            value: 'Kids Academy happening',
+            important: false,
+        },
+    ]
+},
+{
+    key: '2018-10-1',
+    toDo: [{
+            value: 'buy tobacco',
+            important: false,
+        },
+        {
+            value: 'make shisha',
+            important: false,
+        },
+        {
+            value: 'chill',
+            important: false,
+        },
+    ]
 }];
-export { toDosDataInfo };
+export {toDosDataInfo}
