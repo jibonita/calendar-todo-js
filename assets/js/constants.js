4c62f2b186c623123ee250a370dbebd93e0ae055
jibonita <bamishore@gmail.com>
2018-10-14 10:38:31 +0300

    added constant TODO_CONTAINER

--- assets/js/constants.js
+++ assets/js/constants.js
@@ -2,7 +2,7 @@ const constants = {
   CELL_PERCENTAGE_WIDTH: 14,
   CALENDAR_CONTAINER_ID: '#cal-container',
   CALENDAR_MAIN_CONTAINER: '#main-calendar-container',
-  TODO_CONTAINER: '#container',
+  TODO_CONTAINER: '#todo-container',
   ENTER_BUTTON_EVENT_CODE: 13,
 };
 
