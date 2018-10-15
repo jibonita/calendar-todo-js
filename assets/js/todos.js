f12e629ee3f88cf56bc2853984ea680bcd4221d0
Stefan Georgiev <s.georgiev.007@gmail.com>
2018-10-15 12:48:04 +0300

    fixed ton of shit(toDo functions, events, etc.)

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,25 +1,31 @@
 /* global $ */
 import {
 	toDoElement,
-	DatabaseProcesses,
+	DatabaseProcesses
 } from './dbProcesses.js';
 import {
-	constants,
+	constants
 } from './constants.js';
 
-const getTaskHTMLCode = (text, importancyFlag) => {
-	const important = !importancyFlag? 'important' : '';
-	return '<li><fake /><span class="trashcontainer">'
-		+ '<ion-icon name="trash" role="img" class="trash"></ion-icon></span>'
-		+ text
-		+ '<span class="starcontainer">'
-		+ `<ion-icon name="star" role="img" class="${important}star" ></ion-icon>`
-		+ '</span></li>';
-};
+const trueOrFalseImportant = (condition, element, textToAppend) => {
+	if (condition) {
+		element.replaceWith(
+			'<li><span class=\'trash\'><i class=\'fa fa-trash\'></i></span>' +
+			textToAppend +
+			'<span class=\'star important\'><i class=\'fa fa-star\'></i></span></li>'
+		);
+	} else {
+		element.replaceWith(
+			'<li><span class=\'trash\'><i class=\'fa fa-trash\'></i></span>' +
+			textToAppend +
+			'<span class=\'star\'><i class=\'fa fa-star\'></i></span></li>'
+		);
+	}
+
+}
 
 const findClickedElementIndex = (textToFind) => {
 	let ClickedElementIndex = 0;
-	// console.log(toDoElement.toDo, textToFind);
 	toDoElement.toDo.filter((element, i) => {
 		if (element.value === textToFind) {
 			ClickedElementIndex = i;
@@ -27,115 +33,119 @@ const findClickedElementIndex = (textToFind) => {
 	});
 	return ClickedElementIndex;
 };
-
 const todoDataVisualization = (todoObj) => {
+
 	if (todoObj.toDo.length !== 0) {
 		for (let i = 0; i < todoObj.toDo.length; i++) {
 			const isImportant = todoObj.toDo[i].important;
-
-			$('#toDos').append(getTaskHTMLCode(todoObj.toDo[i].value, isImportant));
-			// if (isImportant) {
-			// 	$('#toDos').append(
-			// 		'<li><span><ion-icon name="trash" role="img" class="trash"></ion-icon></span>' +
-			// 		todoObj.toDo[i].value +
-			// 		'<span><ion-icon name="star" role="img" class="star"></ion-icon></span></li>'
-			// 	);
-			// } else {
-			// 	$('#toDos').append(
-			// 		'<li><fake /><span class="trashcontainer"><ion-icon name="trash" role="img" class="trash"></ion-icon></span>' +
-			// 		todoObj.toDo[i].value +
-			// 		'<span class="starcontainer"><ion-icon name="star" role="img" class="star" ></ion-icon></span></li>'
-			// 	);
-			// }
+			if (isImportant) {
+				$('#toDos').append(
+					'<li><span class=\'trash\'><i class=\'fa fa-trash\'></i></span>' +
+					todoObj.toDo[i].value +
+					'<span class=\'star important\'><i class=\'fa fa-star\'></i></span></li>'
+				);
+			} else {
+				$('#toDos').append(
+					'<li><span class=\'trash\'><i class=\'fa fa-trash\'></i></span>' +
+					todoObj.toDo[i].value +
+					'<span class=\'star\'><i class=\'fa fa-star\'></i></span></li>'
+				);
+			}
 		}
 	}
 };
 let newToDoTaskText;
 
 const editToDoHandler = (event) => {
+
 	newToDoTaskText = $(event.target).text();
-	const input = `<input class='addToDo' id='editInput'
+	const input = `<input maxlength="20" class='addToDo' id='editInput'
 	type='text' value='${newToDoTaskText}'>`;
 	$(event.target).replaceWith(input);
 	$('#editInput').focus();
+	$('#editInput').blur(() => {
+		let index = findClickedElementIndex(newToDoTaskText)
+
+		trueOrFalseImportant(toDoElement.toDo[index].important == true, $('#editInput'), newToDoTaskText)
+	});
+
 };
 
-const endOfEditHandler = (event) => {
+
+
+const endOfEditHandler = function (event) {
+
+
+	//===================================================================================================
 	if (event.which === constants.ENTER_BUTTON_EVENT_CODE) {
 		const toDoNewText = $(this).val();
+		let index = findClickedElementIndex(newToDoTaskText)
 		DatabaseProcesses
-			.editToDo(findClickedElementIndex(newToDoTaskText), toDoNewText);
+			.editToDo(index, toDoNewText);
 		$(this).val('');
+		console.log(toDoElement.toDo);
+		console.log(index);
 
-		// TODO: refactor with function getTaskHTMLCode(text, importancyFlag)
-		$(this).replaceWith(
-			'<li><span></span><span class="trashcontainer"><ion-icon name="trash" class="trash"></ion-icon></span>' +
-			toDoNewText +
-			'<span class="starcontainer"><ion-icon name="star" role="img" class="star" ></ion-icon></span></li>'
-		);
+		trueOrFalseImportant(toDoElement.toDo[index].important == true, $(this), toDoNewText)
+
 	}
 };
-
-const addNewToDoHandler = (event) => {
-	if (event.which === constants.ENTER_BUTTON_EVENT_CODE) {
-		const todoText = $(event.target).val();
-		$(event.target).val('');
-
-		// TODO: refactor using function getTaskHTMLCode(text, importancyFlag)
-		$('ul').append(
-			'<li><span></span><span class="trashcontainer"><ion-icon name="trash" class="trash"></ion-icon></span>' +
-			todoText +
-			'<span class="starcontainer"><ion-icon name="star" role="img" class="star" ></ion-icon></span></li>'
-		);
-		DatabaseProcesses.pushToDate(todoText);
-	}
-};
-
-const toggleInputHandler = () => {
-	$('input.addToDo').fadeToggle(300, () => $(this).toggleClass('.hide'));
-};
-
 const deleteToDoTaskHandler = (event) => {
-	const toDoToDelete = $(event.target).closest('li');
+
+	let toDoToDelete = $(event.target).closest('li');
+
 	const toDoToDeleteContent = toDoToDelete.text();
+	console.log(toDoToDeleteContent)
 
 	const index = findClickedElementIndex(toDoToDeleteContent);
 	DatabaseProcesses.deleteToDo(index);
 	toDoToDelete
-		.fadeOut(500, function() {
+		.fadeOut(500, function () {
 			$(this).remove();
 		});
 	event.stopPropagation();
 };
+const addNewToDoHandler = (event) => {
+	if (event.which === constants.ENTER_BUTTON_EVENT_CODE) {
+		const todoText = $(event.target).val();
+		$(event.target).val('');
+		$('ul').append(
+			'<li><span class=\'trash\'><i class=\'fa fa-trash\'></i></span>' +
+			todoText +
+			'<span class=\'star\'><i class=\'fa fa-star\'></i></span></li>'
+		);
+		DatabaseProcesses.pushToDate(todoText);
+	}
+};
+const toggleInputHandler = () => {
+	$('input.addToDo').fadeToggle(300, () => $(this).toggleClass('.hide'));
+};
 
 const toggleImportancyHandler = (event) => {
-	// const importancyStarText = $(event.target).parent().parent().parent().text();
 	const importancyStarText = $(event.target).closest('li').text();
 	const index = findClickedElementIndex(importancyStarText);
 	DatabaseProcesses.editImportance(index);
-	// $(event.target).toggleClass('important');
-	$(event.target).toggleClass('star importantstar');
+	$(event.target).closest('span').toggleClass('important');
+	console.log(toDoElement.toDo);
+
 	event.stopPropagation();
 };
-
 const toggleCalendar = () => {
 	$('ul').empty();
 	$(constants.TODO_CONTAINER).trigger('todoclosed');
 };
-
-const setToDoEvents = () => {
+const setToDoEvents = function () {
 	$('ul').on('click', 'li', editToDoHandler);
 	$('ul').on('keypress', 'input', endOfEditHandler);
+	$('ul').on('click', 'span.trash', deleteToDoTaskHandler);
 	$('input.addToDo').keypress(addNewToDoHandler);
 	$('#toggle-form').on('click', toggleInputHandler);
-	$('ul').on('click', '.trashcontainer', deleteToDoTaskHandler);
-	$('ul').on('click', '.starcontainer', toggleImportancyHandler);
+	$('ul').on('click', '.svg-inline--fa.fa-star', toggleImportancyHandler);
 	$('#back-to-calendar').on('click', toggleCalendar);
 };
-
 export {
-	todoDataVisualization,
+	todoDataVisualization
 };
 export {
-	setToDoEvents,
+	setToDoEvents
 };
